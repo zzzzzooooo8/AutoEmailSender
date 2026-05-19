3d1ba96b82c61b7e4a43f7b0f35a462ac54b358f
JunieXD <1240840076@qq.com>
2026-05-19 18:17:32 +0800

    fix(frontend): 兼容新版 hooks lint 规则

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,5 +19,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'react-hooks/refs': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+    },
   },
 ])
